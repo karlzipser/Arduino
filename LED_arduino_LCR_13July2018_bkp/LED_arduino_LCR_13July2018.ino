dca8e555fd9a0b87b2026abf8cd11e801c4b9c19
Karl Zipser <karlzipser@Karls-MacBook.local>
2018-07-14 21:02:08 -0700

    gacp

--- LED_arduino_LCR_13July2018_bkp/LED_arduino_LCR_13July2018.ino
+++ LED_arduino_LCR_13July2018_bkp/LED_arduino_LCR_13July2018.ino
@@ -9,39 +9,36 @@
 
 Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
 
-// [orientation][blink][color][_,symbol]
-// e.g., 12304 = straight, blink=2, yellow, play
 
 const int STRAIGHT = 1;
 const int LEFT = 2;
 const int RIGHT = 3;
-
-const int BLINK_0 = 0;
-const int BLINK_1 = 1;
-const int BLINK_2 = 2;
-
-const int RED = 1;
-const int GREEN = 2;
-const int YELLOW = 3;
+const int SELECTED = 1;
+const int NOT_SELECTED = 2;
 
 const int DIRECT = 1;
 const int FOLLOW = 2;
 const int FURTIVE = 3;
 const int PLAY = 4;
+
 const int CALIBRATE = 5;
-const int DRIVE_MODE = 6;
-const int SELECT_MODE = 7;
+const int DRIVE_MODE = 15;
+const int SELECT_MODE = 16;
+
+
+const int HUMAN = 6;
+const int NETWORK = 7;
+
 const int LOCAL = 8;
 const int HOME = 9;
 const int TILDEN = 10;
 const int CAMPUS = 11;
 const int ARENA = 12;
 const int OTHER = 13;
-//const int AGENT = 14;
+
+const int AGENT = 14;
+
 const int LINE = 15;
-const int LINE_BUTTON_4 = 16;
-const int HUMAN = 17;
-const int NETWORK = 18;
 
 static const uint8_t PROGMEM //type for bitmaps
 Calibrate_bmp[] =
@@ -71,24 +68,6 @@ select_mode_bmp[] =
   B11111011,
   B01111011,
   B00110000 },
-human_bmp[] =
-{ B00000000,
-  B00000000,
-  B11111110,
-  B00010000,
-  B00010000,
-  B11111110,
-  B00000000,
-  B00000000 },
-network_bmp[] =
-{ B00000000,
-  B00000000,
-  B11111110,
-  B00110000,
-  B1100000,
-  B11111110,
-  B00000000,
-  B00000000 },
 _bitmap[] =
 { B00000000,
   B00000000,
@@ -280,15 +259,6 @@ agent_bmp[] =
   B11011111,
   B01101110,
   B00000000 },
-line_4_bmp[] =
-{ B00000000,
-  B00000001,
-  B00000001,
-  B11111101,
-  B00100001,
-  B11100001,
-  B00000001,
-  B00000000 },
 line_bmp[] =
 { B00000000,
   B00000001,
@@ -298,7 +268,6 @@ line_bmp[] =
   B00000001,
   B00000001,
   B00000000 };
-
 
 
 void setup() {
@@ -331,107 +300,98 @@ void loop() {
   unsigned int parsed_int = Serial.parseInt();
 
   if (parsed_int > 0) {
-      int orientation = (parsed_int)/10000;
-      int blink =       (parsed_int-10000*orientation)/1000;
-      int color =       (parsed_int-10000*orientation-1000*blink)/100;
-      int symbol =      (parsed_int-10000*orientation-1000*blink-100*color)/1;
+      int arrow_direction_or_select = parsed_int/10000;
+      int agent = (parsed_int-arrow_direction_or_select*10000)/100;
+      int behavioral_mode_or_place = (parsed_int-agent*100-arrow_direction_or_select*10000);
 
-    Serial.print(orientation);
-    Serial.print(",");
-    Serial.print(blink);
-    Serial.print(",");
-    Serial.print(color);
-    Serial.print(",");
-    Serial.println(symbol);
-    
+    //Serial.print(arrow_direction_or_select);
+    //Serial.print(",");
+    //Serial.print(agent);
+    //Serial.print(",");
+    //Serial.println(behavioral_mode_or_place);
 
-
-    if (blink == BLINK_0) {
-      blink_rate = 0;
-    } else if (blink == BLINK_1) {
-      blink_rate = 1;
-    } else if (blink == BLINK_2) {
-      blink_rate = 2;
-    }
-
-    if (color == RED) {
+    if (agent == HUMAN) {
       led_color = LED_RED;}
-    else if (color == GREEN) {
+    else if (agent == NETWORK) {
       led_color = LED_GREEN;}
-    else if(color == YELLOW)
-      {led_color = LED_YELLOW;}
+    else {led_color = LED_YELLOW;}
 
+    if (arrow_direction_or_select == STRAIGHT) {
+      blink_rate = 0;
+    } else if (arrow_direction_or_select == LEFT) {
+      blink_rate = 1;
+    } else if (arrow_direction_or_select == RIGHT) {
+      blink_rate = 1;
+    } else if (arrow_direction_or_select == SELECTED) {
+      blink_rate = 0;
+    } else if (arrow_direction_or_select == NOT_SELECTED) {
+      blink_rate = 1;
+    }
     matrix.clear();
 
-    if (symbol == LINE) {
+    if (behavioral_mode_or_place == LINE) {
       matrix.drawBitmap(0, 0, line_bmp, 8, 8, led_color);
-    } else if (symbol == CALIBRATE) {
+    } else if (behavioral_mode_or_place == CALIBRATE) {
       matrix.drawBitmap(0, 0, Calibrate_bmp, 8, 8, led_color);
-    } else if (symbol == DRIVE_MODE) {
+    } else if (behavioral_mode_or_place == DRIVE_MODE) {
       matrix.drawBitmap(0, 0, drive_mode_bmp, 8, 8, led_color);
-    } else if (symbol == SELECT_MODE) {
+    } else if (behavioral_mode_or_place == SELECT_MODE) {
       matrix.drawBitmap(0, 0, select_mode_bmp, 8, 8, led_color);
-    } else if (symbol == LINE_BUTTON_4) {
-      matrix.drawBitmap(0, 0, line_4_bmp, 8, 8, led_color);
-    } else if (symbol == HUMAN) {
-      matrix.drawBitmap(0, 0, human_bmp, 8, 8, led_color);
-    } else if (symbol == NETWORK) {
-      matrix.drawBitmap(0, 0, network_bmp, 8, 8, led_color);
-    } else if (symbol == DIRECT) {
-        if (orientation == STRAIGHT) {
+    } else if (behavioral_mode_or_place == DIRECT) {
+        if (arrow_direction_or_select == STRAIGHT) {
           matrix.drawBitmap(0, 0, direct_straight_bmp, 8, 8, led_color);
-        } else if (orientation == LEFT) {
+        } else if (arrow_direction_or_select == LEFT) {
           matrix.drawBitmap(0, 0, direct_left_bmp, 8, 8, led_color);
-        } else if (orientation == RIGHT) {
+        } else if (arrow_direction_or_select == RIGHT) {
           matrix.drawBitmap(0, 0, direct_right_bmp, 8, 8, led_color);
         }
-    } else if (symbol == FOLLOW) {
-        if (orientation == STRAIGHT) {
+    } else if (behavioral_mode_or_place == FOLLOW) {
+        if (arrow_direction_or_select == STRAIGHT) {
           matrix.drawBitmap(0, 0, follow_straight_bmp, 8, 8, led_color);
-        } else if (orientation == LEFT) {
+        } else if (arrow_direction_or_select == LEFT) {
           matrix.drawBitmap(0, 0, follow_left_bmp, 8, 8, led_color);
-        } else if (orientation == RIGHT) {
+        } else if (arrow_direction_or_select == RIGHT) {
           matrix.drawBitmap(0, 0, follow_right_bmp, 8, 8, led_color);
         }
-    } else if (symbol == FURTIVE) {
-        if (orientation == STRAIGHT) {
+    } else if (behavioral_mode_or_place == FURTIVE) {
+        if (arrow_direction_or_select == STRAIGHT) {
           matrix.drawBitmap(0, 0, furtive_straight_bmp, 8, 8, led_color);
-        } else if (orientation == LEFT) {
+        } else if (arrow_direction_or_select == LEFT) {
           matrix.drawBitmap(0, 0, furtive_left_bmp, 8, 8, led_color);
-        } else if (orientation == RIGHT) {
+        } else if (arrow_direction_or_select == RIGHT) {
           matrix.drawBitmap(0, 0, furtive_right_bmp, 8, 8, led_color);
         }
-    } else if (symbol == PLAY) {
-        if (orientation == STRAIGHT) {
+    } else if (behavioral_mode_or_place == PLAY) {
+        if (arrow_direction_or_select == STRAIGHT) {
           matrix.drawBitmap(0, 0, play_straight_bmp, 8, 8, led_color);
-        } else if (orientation == LEFT) {
+        } else if (arrow_direction_or_select == LEFT) {
           matrix.drawBitmap(0, 0, play_left_bmp, 8, 8, led_color);
-        } else if (orientation == RIGHT) {
+        } else if (arrow_direction_or_select == RIGHT) {
           matrix.drawBitmap(0, 0, play_right_bmp, 8, 8, led_color);
         }
 
-    } else if (symbol == LOCAL) {
+    } else if (behavioral_mode_or_place == LOCAL) {
           matrix.drawBitmap(0, 0, local_bmp, 8, 8, led_color);
-    } else if (symbol == HOME) {
+    } else if (behavioral_mode_or_place == HOME) {
           matrix.drawBitmap(0, 0, home_bmp, 8, 8, led_color);
-    } else if (symbol == TILDEN) {
+    } else if (behavioral_mode_or_place == TILDEN) {
           matrix.drawBitmap(0, 0, Tilden_bmp, 8, 8, led_color);
-    } else if (symbol == CAMPUS) {
+    } else if (behavioral_mode_or_place == CAMPUS) {
           matrix.drawBitmap(0, 0, campus_bmp, 8, 8, led_color);
-    } else if (symbol == ARENA) {
+    } else if (behavioral_mode_or_place == ARENA) {
           matrix.drawBitmap(0, 0, arena_bmp, 8, 8, led_color);
-    } else if (symbol == OTHER) {
+    } else if (behavioral_mode_or_place == OTHER) {
           matrix.drawBitmap(0, 0, other_bmp, 8, 8, led_color);
 
-    //} else if (symbol == AGENT) {
-    //      matrix.drawBitmap(0, 0, agent_bmp, 8, 8, led_color);
+    } else if (behavioral_mode_or_place == AGENT) {
+          matrix.drawBitmap(0, 0, agent_bmp, 8, 8, led_color);
     }
 
     matrix.writeDisplay();
     matrix.blinkRate(blink_rate);
-  
+
   }
-  
+ 
   delay(10);
 }
 
